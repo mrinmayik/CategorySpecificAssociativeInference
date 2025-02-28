8e79a3f873b7af04f39efd489df0faa0a9a24413
Mrinmayi <mrinmayi.17@gmail.com>
2025-02-28 10:49:01 +0530

    Add functions and ggplot themes

--- InitialisePaths.R
+++ InitialisePaths.R
@@ -1,7 +1,8 @@
 #!/bin/bash Rscript
 
 # Check if packages need to be installed
-required_packages <- c("stringr", "tidyr", "dplyr", "ggplot2", "ggpubr", "ez", "rstatix")
+required_packages <- c("stringr", "tidyr", "dplyr", "ggplot2", "ggpubr", "ez", "rstatix",
+                       "psychReport")
 to_be_installed <- required_packages[!(required_packages %in% rownames(installed.packages()))]
 if(length(to_be_installed) > 0){
   for(package in to_be_installed){
@@ -16,6 +17,7 @@ library(ggplot2)
 library(ggpubr)
 library(ez)
 library(rstatix)
+library(psychReport)
 
 # Setup paths where scripts are
 scripts_path <- "~/GitDir/CodeWithPapers/CategorySpecificAssociativeInference"
@@ -32,10 +34,28 @@ participants <- read.table(paste0(data_path,
                            header = TRUE)
 aim_participants <- participants %>% 
   filter(AIM == "Completed") %>% 
-  pull(subj_id)
+  pull(participant_id)
 loc_participants <- participants %>% 
   filter(Localiser == "Completed") %>% 
-  pull(subj_id)
+  pull(participant_id)
+
+factor_levels <- list(category = list(levels = c("scene", "face"),
+                                      labels = c("Scene", "Face"),
+                                      colours = c("#66CC00", "#0099FF")),
+                      trialtype_study = list(levels = c("AB", "BC"),
+                                             labels=c("AB", "BC")),
+                      trialtype_test = list(levels = c("AB", "BC", "AC"),
+                                            labels=c("AB", "BC", "AC")),
+                      hemisphere = list(levels = c("L", "R", "Collapsed"),
+                                        labels = c("Left", "Right", "Collapsed")),
+                      category_localiser = list(levels = c("scene", "face", "obj"),
+                                                labels = c("Scene", "Face", "Object")),
+                      studyitem_pairtype = list(levels = c("Unique_AB", "Linking_AB", 
+                                                           "Linking_BC", "Unique_BC"),
+                                                labels = c("A\n(object)", "B\n(scene/face)", 
+                                                         "B\n(scene/face)", "C\n(object)")))
+
+
 
 # Function to correct for p-values
 correct_p_vals <- function(df, pvalcol, usemethod="fdr"){
@@ -50,3 +70,31 @@ correct_p_vals <- function(df, pvalcol, usemethod="fdr"){
   return(df)
 }
 
+summarise_data <- function(df, col_name, rm_na = FALSE){
+  df <- as.data.frame(df)
+  M <- mean(df[,col_name], na.rm = rm_na)
+  SD <- sd(df[,col_name], na.rm = rm_na)
+  SE <- SD / sqrt(nrow(df))
+  LCI <- M - 1.96*SE
+  HCI <- M + 1.96*SE
+  MeanPlusSE <- M+SE
+  MeanMinusSE <- M-SE
+  NumOfRows <- nrow(df)
+  data.frame(Mean=M, SD=SD, SE=SE, LCI=LCI, HCI=HCI, MeanPlusSE=MeanPlusSE, MeanMinusSE, Rows=NumOfRows)
+}
+
+
+paper_facet_theme <- theme(strip.text.x = element_text(size = 22, colour = "black"),
+                           strip.text.y = element_text(size = 22, colour = "black"), 
+                           strip.background = element_rect(color="white", fill="white", linewidth=1.5, linetype="solid"),
+                           panel.border = element_rect(colour = "black", fill = NA, linewidth=1.5))
+x_axis_theme <- theme(axis.title.x = element_blank(), 
+                      axis.text.x = element_text(colour = "#000000", size = 18)) 
+y_axis_theme <-   theme(axis.title.y = element_text(colour = "#000000", size = 22), 
+                        axis.text.y = element_text(colour = "#000000", size = 18))
+blank_bg_theme <- theme(panel.grid.major = element_blank(), 
+                        panel.grid.minor = element_blank(),
+                        panel.background = element_blank(), 
+                        axis.line = element_line(colour = "black"))
+legend_theme <- theme(legend.text = element_text(face = "bold", size = 20), 
+                      legend.title = element_text(face = "bold", size = 25))
