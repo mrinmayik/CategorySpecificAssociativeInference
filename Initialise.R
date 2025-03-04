f94281c0cb76bfa619e505404dbfcbf74498ce97
Mrinmayi <mrinmayi.17@gmail.com>
2025-03-04 16:57:03 +0530

    Add factor levels, comments, etc

--- Initialise.R
+++ Initialise.R
@@ -31,7 +31,7 @@ scripts_path <- "~/GitDir/CodeWithPapers/CategorySpecificAssociativeInference"
 base_path <- "~/Desktop/AssociativeInference/"
 data_path <- paste0(base_path, "Defaced/")
 derivatives_path <- paste0(data_path, "derivatives/")
-deconvolve_path <- paste0(data_path, "DeconvolveOutput/")
+deconvolve_path <- paste0(derivatives_path, "DeconvolveOutput/")
 
 ################## Participant Management ##################
 # Read and filter participant information based on task completion
@@ -47,25 +47,34 @@ loc_participants <- participants %>%
 
 ################## Factor Level Definitions ##################
 # Define factor levels and labels for different experimental conditions
+# Each list element contains standardized levels, display labels, and (where applicable) color codes
 factor_levels <- list(category = list(levels = c("scene", "face"),
                                       labels = c("Scene", "Face"),
                                       colours = c("#66CC00", "#0099FF")),
-                      trialtype_study = list(levels = c("AB", "BC"),
+                      pairtype_study = list(levels = c("AB", "BC"),
                                              labels=c("AB", "BC")),
                       trialtype_test = list(levels = c("AB", "BC", "AC"),
                                             labels=c("AB", "BC", "AC")),
-                      hemisphere = list(levels = c("L", "R", "Collapsed"),
-                                        labels = c("Left", "Right", "Collapsed")),
+                      trialtype_test_beta = list(levels = c("dir", "indir"),
+                                            labels=c("Direct", "Indirect")),
                       category_localiser = list(levels = c("scene", "face", "obj"),
-                                                labels = c("Scene", "Face", "Object")),
+                                                labels = c("Scene", "Face", "Object"),
+                                                colours = c("#66CC00", "#0099FF", "#ffd100")),
                       studyitem_pairtype = list(levels = c("Unique_AB", "Linking_AB", 
                                                            "Linking_BC", "Unique_BC"),
                                                 labels = c("A\n(object)", "B\n(scene/face)", 
-                                                         "B\n(scene/face)", "C\n(object)")))
+                                                           "B\n(scene/face)", "C\n(object)")),
+                      rois = list(levels = c("Ant Hipp Head", "CA1", "CA23DG", "Sub", "Hipp Tail", 
+                                             "alERC", "pmERC", "PRC", "PHC"),
+                                  roi_type = c(rep_len("HC", 5),
+                                               rep_len("MTL", 4))),
+                      hemisphere = list(levels = c("Left", "Right", "Collapsed"),
+                                        labels = c("Left", "Right", "Bilateral")))
 
 ################## Utility Functions ##################
 # Function to correct p-values using different methods
 correct_p_vals <- function(df, pvalcol, usemethod = "fdr"){
+  df <- as.data.frame(df)
   #Is raw p-value significant?
   df[, "sig"] <- ifelse(df[, pvalcol] <= 0.05, "*", "")
   
@@ -93,17 +102,22 @@ summarise_data <- function(df, col_name, rm_na = FALSE){
 
 ################## Plotting Themes ##################
 # Define consistent themes for paper-quality visualizations
+# Theme for facet labels and panel borders
 paper_facet_theme <- theme(strip.text.x = element_text(size = 22, colour = "black"),
                            strip.text.y = element_text(size = 22, colour = "black"), 
                            strip.background = element_rect(color="white", fill="white", linewidth=1.5, linetype="solid"),
                            panel.border = element_rect(colour = "black", fill = NA, linewidth=1.5))
+# Theme for x-axis formatting
 x_axis_theme <- theme(axis.title.x = element_blank(), 
                       axis.text.x = element_text(colour = "#000000", size = 18)) 
+# Theme for y-axis formatting
 y_axis_theme <-   theme(axis.title.y = element_text(colour = "#000000", size = 22), 
                         axis.text.y = element_text(colour = "#000000", size = 18))
+# Theme for clean background without grid lines
 blank_bg_theme <- theme(panel.grid.major = element_blank(), 
                         panel.grid.minor = element_blank(),
                         panel.background = element_blank(), 
                         axis.line = element_line(colour = "black"))
+# Theme for legend formatting
 legend_theme <- theme(legend.text = element_text(face = "bold", size = 20), 
                       legend.title = element_text(face = "bold", size = 25))
